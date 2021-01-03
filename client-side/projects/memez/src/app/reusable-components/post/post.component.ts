2a35f245ea8f8c4175ea12f863da3d1ae8e79e06
orialhad <orialhad@gmail.com>
2021-01-03 03:40:48 +0200

    fixed pics css

--- client-side/projects/memez/src/app/reusable-components/post/post.component.ts
+++ client-side/projects/memez/src/app/reusable-components/post/post.component.ts
@@ -2,6 +2,9 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {IPost} from '../../../../../../../sheard/interfaces/IPost';
 import {IUser} from '../../../../../../../sheard/interfaces/IUser';
 import {DomSanitizer} from '@angular/platform-browser';
+import {HttpEvent} from '@angular/common/http';
+
+
 
 
 @Component({
@@ -9,15 +12,20 @@ import {DomSanitizer} from '@angular/platform-browser';
   templateUrl: './post.component.html',
   styleUrls  : ['./post.component.css']
 })
+
+
+
+
 export class PostComponent implements OnInit {
   @Input() post: IPost;
   @Input() currentUser :IUser;
   @Output() postLiked = new EventEmitter();
   @Output() PostToDelete = new EventEmitter();
 
-
   @Input() selected: boolean;
   @Output() selectedChange = new EventEmitter<boolean>();
+
+  status: boolean
 
 
   constructor() {
@@ -37,6 +45,13 @@ export class PostComponent implements OnInit {
   public toggleSelected() {
     this.selected = !this.selected;
     this.selectedChange.emit(this.selected);
+  }
+
+  displayOriginal(){
+    this.status = true
+  }
+  closeModal(){
+    this.status = false
   }
 
 }
